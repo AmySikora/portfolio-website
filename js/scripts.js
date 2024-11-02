9b3b234571e2cdb71f0d3389a310d364161d0c39
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-02 14:42:31 -0700

    Updated styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("html-progress-bar").style.width = "40%"; // Example: 40% for HTML
-    document.getElementById("css-progress-bar").style.width = "30%";  // Example: 30% for CSS
-    document.getElementById("js-progress-bar").style.width = "20%";   // Example: 20% for JavaScript
+$(document).ready(function() {
+    $('.progress').each(function() {
+        $(this).animate({
+            width: $(this).data('percentage') + '%'
+        }, 1000);
+    });
 });
