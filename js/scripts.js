cfe28a04d75ca8226d56d8e6ea4cf660ceea6e1b
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-02 15:14:44 -0700

    trying to fox menu bar

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,17 @@ $(document).ready(function() {
         }, 1000);
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const currentPath = window.location.pathname.split("/").pop();
+    const navLinks = document.querySelectorAll(".navigation-list__item");
+
+    navLinks.forEach(link => {
+        const linkPath = link.getAttribute("href").split("/").pop();
+        if (linkPath === currentPath) {
+            link.classList.add("navigation-list__item--active");
+        } else {
+            link.classList.remove("navigation-list__item--active");
+        }
+    });
+});
