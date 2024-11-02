11ebf860b1c34bacf713f0ff368f3b95f429e73c
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-11-02 14:29:02 -0700

    added scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("html-progress-bar").style.width = "40%"; // Example: 40% for HTML
+    document.getElementById("css-progress-bar").style.width = "30%";  // Example: 30% for CSS
+    document.getElementById("js-progress-bar").style.width = "20%";   // Example: 20% for JavaScript
+});
